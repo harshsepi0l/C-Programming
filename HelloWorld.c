e75efbc98e62ff0cf65553f5a2266f2459f501d7
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2023-07-16 18:07:47 -0400

    changes

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 // Structures
     struct Rectangle
@@ -39,12 +40,25 @@
         }
     
 //Arrays - Parameter passing (Arrays are always pass-by-address)  
-void fun(int parameterArray[], int n){
+void fun(int * parameterArray, int n){
+    //You can use a star or brackets to initialize an array.
+    //You are doing nothing but pointing a varible to an array.
+    parameterArray[0] = 15;
     printf("\n\nParameter Array: ");
     for (int i = 0; i < n; i++ ){
         printf(" %d", parameterArray[i]);
     }
 }
+
+    //Array passing with return types.
+    int * funTwo(int size){
+        int *twoPointer ;
+        twoPointer = malloc(sizeof(*twoPointer)); //Created in heap
+        for(int i = 0; i < size; i++){
+            twoPointer[i] = i + 1;
+        }
+        return twoPointer;
+    }
 
 // Main function:
 
@@ -182,14 +196,22 @@ Escape sequence = Character combination consisting of a backslash
 
 
 
-//Arrays as parameter (Arrays are always call-by-address)
-int parameterArray[] = {2, 4, 6, 8, 10};
-int n = 5;
+//Array as parameter (Arrays are always call-by-address)
+    int parameterArray[] = {2, 4, 6, 8, 10};
+    int n = 5;
 
-for (int i; i < n; i++ ){
+    for (int i; i < n; i++ ){
     printf("\n%d", parameterArray[i]);
 }
-fun(parameterArray, 5);
+    fun(parameterArray, n);
+
+//Array return type 
+    int *returnPointer, sz=5;
+    returnPointer = funTwo(sz);
+    printf("\nReturn function array: ");
+    for(int i = 0; i < sz; i++){
+        printf("%d ", returnPointer[i]);
+    }
 
 
  return 0; // always have to return something so just return 0 lol
