9028f33186099cc49af070cabae7b6a6946cba40
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2023-07-16 16:41:18 -0400

    Changes

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-
 // Structures
     struct Rectangle
     {
@@ -9,8 +8,45 @@
         int breadth;
     };
 
+
+
+// Functions - Parameter Passing
+    // Using Pass by Value and Address (No Reference in C)
+    // Declaration of a function:
+
+    //Pass by Value
+    int add (int first, int second) // Prototype or Signature of a function
+            //Formal Parameters
+    {
+        int added;
+        added = first + second;
+        return (added);   
+    }
+
+    //Pass by address/reference(in C) - Used to change the actual parameters instead of formal
+     // --> Makes use of pointers to do this
+
+        void Swap (int *j, int *k) //Swap function 
+        {
+            int temp;       //Temp val
+            temp = *j;      // Temp val stores j
+            *j = *k;        // j now equals k
+            *k = temp;      // k now takes temp which is original j
+
+            //Since we are not returning anything and manipulating
+            //the parameters we don't need a return call. (function is 
+            //also void).
+        }
+    
+//Arrays - Parameter passing (Arrays are always pass-by-address)  
+    void fun(int sizeArray[])
+    {
+        printf(sizeof(sizeArray)/sizeof(int));
+    }
+
+// Main function:
+
 int main(){
-    printf("Hello World!\n");
 
 /* 
 Escape sequence = Character combination consisting of a backslash 
@@ -117,14 +153,41 @@ Escape sequence = Character combination consisting of a backslash
     //.. Pointers to Structs
         struct Rectangle r;
         struct Rectangle *t = &r;
+        
 
         t -> length = 20;
         t -> breadth = 10;
 
         printf("\nValue of Rectangle using pointer: %d", *t);
 
+//Functions
+    //.. Call by Value
+        int numberOne, numberTwo, finalThree;
+        numberOne = 10;
+        numberTwo = 5;
+
+        finalThree = add(numberOne,numberTwo); //Actual Parameters
+        printf("\nSum of both numbers: %d", finalThree);
+    
+    //.. Call by Address
+        int addressOne, addressTwo;
+        addressOne = 10;
+        addressTwo = 20; 
+        printf("\nBefore Swapping: %d, %d", addressOne, addressTwo);
+
+        Swap(&addressOne, &addressTwo); // Ampersand to send the address of the variables!!!
+        printf("\nAfter swapping: %d, %d", addressOne, addressTwo);
 
 
+
+//Arrays as parameter (Arrays are always call-by-address)
+int pointerArray[] = {2, 4, 6, 8, 10};
+int n = 5;
+
+for (int i; i < n; i++ ){
+    printf("\n%d", pointerArray[i]);
+}
+fun(pointerArray);
 
  return 0; // always have to return something so just return 0 lol
 }
