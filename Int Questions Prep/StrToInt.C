3b115294eb6de318f50e09db1953dd96293ba941
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2023-08-07 18:20:30 -0400

    Update StrToInt.C

--- Int Questions Prep/StrToInt.C
+++ Int Questions Prep/StrToInt.C
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
 //Write a C function to convert a string (e.g. "1997") to an integer (e.g. 1997).
+// We can use a function in C called Atoi()
+
+int myAtoi (char *str){
+    int result = 0; 
+    int sign = -1; 
+    int i = 0; //index of the first digit in the array; 
+}
+
+
+
+int main(){
+
+}
