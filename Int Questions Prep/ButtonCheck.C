9cbaf9530c056eb17a6249e65ed4dd704e50737e
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2023-08-08 21:14:24 -0400

    Update ButtonCheck.C

--- Int Questions Prep/ButtonCheck.C
+++ Int Questions Prep/ButtonCheck.C
@@ -3,3 +3,26 @@ Write a C function to detect if a button is pressed or not. The button must be p
 In other words, the function starts with the button being unpressed. Every "check" happens in 10ms intervals. With each "check", the button will either 
 be pressed or unpressed. You must wait for 5 "checks" (50ms) of continuous pressed/unpressed status updates before you update the output.
 */
+
+// if button is pressed = 1, and if button is NOT pressed = 0
+
+int buttonCheck ( ){
+    int unpressed = 0;
+    int pressed = 1;
+    int check = ;
+    int i = 0; 
+
+    for (; check > 5; check ++ ){
+        check=+ i;
+    }
+    for (int i = 0; i < 10; i++){
+        if (check == 5)
+
+    }
+}
+
+
+int main(){
+
+    return 0;
+}
